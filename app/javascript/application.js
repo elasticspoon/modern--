c12bbf7b9c2451fab6245652d6e8112edb6531d2
elasticspoon <mail@yuribocharov.dev>
2024-08-05 19:40:40 -0400

    shit is busted

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,18 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./components/venue_display"
+import "animate.css"
+
+document.addEventListener("turbo:before-stream-render", (event) => {
+  if (event.target.action === "remove") {
+    const targetFrame = document.getElementById(event.target.target)
+    if (targetFrame.dataset.animateOut) {
+      event.preventDefault()
+      const element = targetFrame
+      element.classList.add(targetFrame.dataset.animateOut)
+      element.addEventListener("animationend", () => {
+        targetFrame.remove()
+      })
+    }
+  }
+})
